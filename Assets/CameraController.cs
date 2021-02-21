e9a98612938e0d380e57df75f479c2efe00cf53d
John <leggojohn.js@gmail.com>
2021-02-21 20:33:14 +1100

    add camera controller script

---
+++ Assets/CameraController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] float lerpTime = 0.5f;
+
+    void FixedUpdate()
+    {
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, player.transform.position + Vector3.back, lerpTime);
+    }
+}
