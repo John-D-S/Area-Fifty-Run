378079295677e6001853b2b1fb042c56a6e7893c
John <68535837+John-D-S@users.noreply.github.com>
2021-03-14 21:14:59 +1100

    camera now scales based on player speed.

--- Assets/AreaFiftyRun/Scripts/CameraController.cs
+++ Assets/AreaFiftyRun/Scripts/CameraController.cs
@@ -5,11 +5,35 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] GameObject player;
-    [SerializeField] float lerpTime = 0.5f;
+    [SerializeField] float posLerpTime = 0.5f;
+    [SerializeField] float scaleLerpTime = 0.1f;
+    [SerializeField] float baseScale = 8;
+    [SerializeField] float scaleMultiplier = 5;
+
+    private Camera cameraComponent;
+    private Vector2 lastFramePlayerPosition;
+    Vector3 targetPosition = Vector3.zero;
+    float targetCameraSize = 0;
+
+    void Start()
+    {
+        //playerRigidBody = player.GetComponent<Rigidbody2D>();
+        lastFramePlayerPosition = player.transform.position;
+        cameraComponent = gameObject.GetComponent<Camera>();
+    }
 
     void FixedUpdate()
     {
-        
-        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, player.transform.position + Vector3.back, lerpTime);
+        targetCameraSize = baseScale + Mathf.Abs((((Vector2)player.transform.position - lastFramePlayerPosition) * scaleMultiplier).magnitude);
+        targetPosition = player.transform.position + Vector3.back;
+
+        //Debug.Log(targetScale);
+
+        cameraComponent.orthographicSize = Mathf.Lerp(cameraComponent.orthographicSize, targetCameraSize, scaleLerpTime);
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPosition, posLerpTime);
+
+        Debug.Log(gameObject.transform.localScale);
+
+        lastFramePlayerPosition = player.transform.position;
     }
 }
