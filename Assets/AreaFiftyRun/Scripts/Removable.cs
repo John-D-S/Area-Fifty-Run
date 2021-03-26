43d40618ebcbfde6305cfb0d2e45b474aad1bec3
John <68535837+John-D-S@users.noreply.github.com>
2021-03-26 20:44:26 +1100

    Fixed the script that removes objects

--- Assets/AreaFiftyRun/Scripts/Removable.cs
+++ Assets/AreaFiftyRun/Scripts/Removable.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class Removable : MonoBehaviour
 {
     private GameObject wallOfDeath;
-    private Vector2 gameObjectOriginalPosition;
 
     public void GetWallOfDeath() 
     {
-        gameObjectOriginalPosition = gameObject.transform.position;
         wallOfDeath = GameObject.FindGameObjectWithTag("WallOfDeath");
     } 
 
@@ -17,7 +15,7 @@ public class Removable : MonoBehaviour
     {
         if (wallOfDeath)
         {
-            if (wallOfDeath.transform.position.x > gameObjectOriginalPosition.x)
+            if (wallOfDeath.transform.position.x > gameObject.transform.position.x)
             {
                 Destroy(gameObject);
             }
